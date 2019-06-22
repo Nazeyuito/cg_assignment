a8455fb094f2be37edf9a7109486f0eb13d2d309
nazeyuito <nazeyui@gmail.com>
2019-06-23 00:21:02 +0900

    sqrtを入れたい

--- cg_assignment/myUtility.h
+++ cg_assignment/myUtility.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <math.h>
 
+//float mySqrt(float x,int ac);//return sqrt(x) (ac = accurate)
 float myfPow(float x, int y);//return x^y (x=float)
 float dintancePoints(float *p1, float *p2,int elm);//return distans point1 to point2 (elm = number of elements)
